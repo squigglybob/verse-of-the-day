0ebfe1169b7d0199457dd086872e95a9d860a4f0
Nathaniel Fenton <nathinabob@gmail.com>
2020-06-24 16:10:02 +0100

    Updates

---
+++ verse-of-the-day/precache-manifest.bee8c2358f47341941ca21d96f4c2917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4758c5bd80022e1dbef5d9481ad57d",
+    "url": "/verse-of-the-day/index.html"
+  },
+  {
+    "revision": "2ce72a88fd757f9585cf",
+    "url": "/verse-of-the-day/static/js/2.0b838182.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/verse-of-the-day/static/js/2.0b838182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e810025d17e1d04cbbc2",
+    "url": "/verse-of-the-day/static/js/main.e752f756.chunk.js"
+  },
+  {
+    "revision": "94e8291c32dedb1f066c",
+    "url": "/verse-of-the-day/static/js/runtime-main.41ad3585.js"
+  }
+]);
